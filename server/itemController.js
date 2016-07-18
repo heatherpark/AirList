865d65158b8ac21a2cdbdc5501fbd8a5bb691316
Sujin Lee <pasdesujin@gmail.com>
2016-07-18 15:17:10 -0400

    create updateAnItem method

--- server/itemController.js
+++ server/itemController.js
@@ -3,7 +3,6 @@ var Q = require('q');
 
 var makeItem = Q.nbind(Item.create, Item);
 var getAll = Q.nbind(Item.find, Item);
-var updateItem = Q.nbind(Item.findOneAndUpdate, Item);
 
 var createItem = function(item, callback) {
   makeItem(item)
@@ -25,11 +24,25 @@ var getAllItems = function(callback) {
   });
 };
 
-// var updateAnItem = function(itemName, newParameters, callback) {
-//   if(newParameters.price) {
-//     updateItem({name:itemName}, {$set: {}})
-//   }
-
-// }
+var updateAnItem = function(itemName, newParams, callback) {
+  Item.findOne({
+    name: itemName
+  }, function(err, doc) {
+    if (newParams.days) {
+        doc.days = newParams.days
+    }
+    if (newParams.price) {
+        doc.price = newParams.price
+    }
+    if (newParams.description) {
+        doc.description = newParams.description
+    }
+    if (newParams.name) {
+        doc.name = newParams.name
+    }
+    doc.save();
+    callback(err, doc)
+  });
+}
 module.exports.createItem = createItem;
 module.exports.getAllItems = getAllItems;
