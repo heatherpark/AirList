172a2c1b7dd1cce0e2813b588dfe0ad8682c01d6
Jeff Lam <jefflam1200@gmail.com>
2016-07-18 11:03:25 -0700

    Create basic angular front-end and added get and post requests to controller

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var app = express();
 
 //for heroku
 var port = process.env.PORT || 9000;
-var mongoUri = process.env.MONGODB_URI || 'mongodb://local/airlistdb';
-mongoose.connect(mongoUri);
+// var mongoUri = process.env.MONGODB_URI || 'mongodb://local/airlistdb';
+// mongoose.connect(mongoUri);
 
 //serve static files
 app.use(express.static(__dirname + '/Client'));
