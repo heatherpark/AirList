17e73c159511bb92eb1e6b25e094d08e4c02b5ee
ChrisHeo <christopher.s.heo@gmail.com>
2016-07-25 20:19:44 -0400

    code cleanup

--- Client/login/loginController.js
+++ Client/login/loginController.js
@@ -1,17 +1,19 @@
 angular.module('app.loginController', ['app'])
 
  .controller('loginController', ['$scope', 'auth', '$window', function ($scope, auth, $window) {
+ //sets the users email address in localStorage. Signs them in
     $scope.login = function(){
       if(!window.localStorage.profile) {
         auth.signin();
       }
     };
-
+//clears the profile and JWT token from localStorage, effectively logging the person out and returning them to the home.html page where it will ask them to log in again.
     $scope.logout = function() {
       window.localStorage.clear();
       $window.location.href ='https://dilp.auth0.com/v2/logout?returnTo=' + $scope.env[0];
    };
 
+//Fired when a user clicks on the 'Contact Owner' button. It will open their default mail client with a prefilled message and subject
    $scope.sendEmail = function(item){
      var subj = "Inquiry regarding " + item.name;
      var message = "Dear item owner,"
--- Client/userAccount/userAccountController.js
+++ Client/userAccount/userAccountController.js
@@ -1,2 +1,4 @@
 angular.module('app.userAccountController', ['app'])
-  //refactor to move functions that belong to userAccount.html into here from homeController.js, which has most of the logic now
+  //refactor to move functions that belong to userAccount.html into here from homeController.js, which has most of the logic
+
+
