5ca2a8a577b997ba83ee5a1c560498c3328d5dd9
Heather Park <hthr.prk@gmail.com>
2016-07-31 13:17:33 -0700

    Inject angular-bootstrap as dependency to 'app'

--- Client/app/app.js
+++ Client/app/app.js
@@ -8,7 +8,8 @@ angular.module('app', [
   'app.controllers',
   'app.factories',
   'angularPayments',
-  'payment'
+  'payment',
+  'ui.bootstrap'
 ])
 
  .config(function myAppConfig ($routeProvider, authProvider){
