2ccb846ce11b273a65a55f947a60fce40c3ff0fb
llo1 <louislo@gmail.com>
2016-07-30 22:02:20 -0700

    Pass image to server and db through item submit form

--- Client/userAccount/userAccountController.js
+++ Client/userAccount/userAccountController.js
@@ -1,4 +1,47 @@
-angular.module('userAccountController', ['app'])
-  //refactor to move functions that belong to userAccount.html into here from homeController.js, which has most of the logic
+angular.module('userAccountController', [])  .directive('appFilereader', function($q) {
+    var slice = Array.prototype.slice;
 
+    return {
+      restrict: 'A',
+      require: '?ngModel',
+      link: function(scope, element, attrs, ngModel) {
+        if (!ngModel) return;
 
+        ngModel.$render = function() {}
+
+        element.bind('change', function(e) {
+          var element = e.target;
+          if(!element.value) return;
+
+          element.disabled = true;
+          $q.all(slice.call(element.files, 0).map(readFile))
+            .then(function(values) {
+              if (element.multiple) ngModel.$setViewValue(values);
+              else ngModel.$setViewValue(values.length ? values[0] : null);
+              element.value = null;
+              element.disabled = false;
+            });
+
+          function readFile(file) {
+            var deferred = $q.defer();
+
+            var reader = new FileReader()
+            reader.onload = function(e) {
+              deferred.resolve(e.target.result);
+            }
+            reader.onerror = function(e) {
+              deferred.reject(e);
+            }
+            reader.readAsDataURL(file);
+
+            return deferred.promise;
+          }
+
+        }); //change
+
+      } //link
+
+    }; //return
+
+  }) //appFilereader
+;
